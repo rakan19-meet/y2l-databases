7695c4cca324b2f1a89961487892fddc3b20dc1e
aboud20-meet <aboud20@meet.mit.edu>
2018-11-01 18:46:11 +0200

    4thsession

--- databases.py
+++ databases.py
@@ -9,17 +9,48 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 # Write your functions to interact with the database here :
+# def add_product(name, price, quality, quantity):
+# 	product_object = Product(
+# 		name= name,
+# 		price= price,
+# 		quality=quality,
+# 		quantity=quantity)
+# 	session.add(product_object)
+# 	session.commit()
 
-def create_product():
+# add_product("lambo", 3, "awesome", 3)
+
+def create_product(name, price, quality, quantity):
+	product_object = Product(
+		name= name,
+		price= price,
+		quality=quality,
+		quantity=quantity)
+	session.add(product_object)
+	session.commit()
+
+# create_product("lambo", 3, "awesome", 3)
+
+  #TODO: complete the functions (you will need to change the function's inputs) 
+def update_product(name, price):
+	product_object=session.query(
+		Product).filter_by(
+		name=name).first()
+	if price > 300:
+		print("too high")
+	else:
+		product_object.price = price	
+		session.commit()
+update_product("kiki", 7)
+
   #TODO: complete the functions (you will need to change the function's inputs)
-  pass
 
-def update_product():
-  #TODO: complete the functions (you will need to change the function's inputs)
-  pass
+def delete_product(product_name):
+	session.query(Product).filter_by(
+		name=product_name).delete()
+	session.commit()
 
-def delete_product(id):
-  pass
+delete_product("lambo")
 
 def get_product(id):
   pass
